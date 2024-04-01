d6cd884de861e8273cbc94f026e8bde415561a22
Damien Sedgwick <damienksedgwick@gmail.com>
2024-04-01 20:12:57 +0100

    added formatting and config for go development

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "eslint", "tailwindcss", "tsserver" }
+local servers = { "eslint", "gopls", "tailwindcss", "tsserver" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -3,6 +3,12 @@ local null_ls = require("null-ls")
 
 local opts = {
   sources = {
+    -- golang
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.goimports_reviser,
+    null_ls.builtins.formatting.golines,
+
+    -- frontend / javascript
     null_ls.builtins.formatting.prettierd,
   },
   on_attach = function(client, bufnr)
